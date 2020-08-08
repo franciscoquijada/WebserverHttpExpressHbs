1e3bee149f1c45d5050b516a81c3da1949caad46
fquijada <fquijada@celcom.cl>
2020-08-08 16:39:38 -0400

    sirviendo pagina estatica con express

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+
+//Sirviendo una pagina estatica con express
+app.use(express.static(__dirname + '/public'));
  
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
     const salida = {
         nombre: 'francisco',
         apellido: 'quijada',
         url: req.url
     };
     res.send(salida);
-});
+});*/
  
 app.listen(8002, ()=>{
     console.log("Escuchando en el puerto 8002");
